2cadd590c19f2e115c888e9337357a20f311b36d
Ori Roitzaid <oriroitzaid@gmail.com>
2023-12-19 15:13:50 +0200

    Added inserting the fields to the database, and the alert dialog

--- app/src/main/java/com/example/ex121/InputStudentActivity.java
+++ app/src/main/java/com/example/ex121/InputStudentActivity.java
@@ -3,10 +3,13 @@ package com.example.ex121;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.DialogInterface;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 /**
  * The student input activity:
@@ -21,6 +24,8 @@ public class InputStudentActivity extends AppCompatActivity {
     EditText etStudName, etAddress, etStudPhone, etHomePhone, etMomName, etMomPhone, etDadName,
             etDadPhone;
     AlertDialog.Builder adb;
+    AlertDialog ad;
+    ContentValues cv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +53,73 @@ public class InputStudentActivity extends AppCompatActivity {
         hlp = new HelperDB(this);
         db = hlp.getWritableDatabase();
         db.close();
+
+        cv = new ContentValues();
     }
 
     public void saveStudent(View view) {
+        if(!areEmptyFields()) {
+            showAlertDialog();
+        }
+        else {
+            Toast.makeText(this, "There is an empty field!", Toast.LENGTH_LONG).show();
+        }
     }
 
     public void showAlertDialog() {
         adb = new AlertDialog.Builder(this);
+        adb.setCancelable(false);
         adb.setTitle("Add New Student");
+        adb.setMessage("Do you want to add the new student?");
 
+        adb.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                saveFieldsToDb();
+            }
+        });
+
+        adb.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        ad = adb.create();
+        ad.show();
+    }
+
+    public void saveFieldsToDb() {
+        cv.clear();
+
+        // Puts the fields to the cv
+        cv.put(Students.ACTIVE, 1);
+        cv.put(Students.STUDENT_NAME, etStudName.getText().toString());
+        cv.put(Students.ADDRESS, etAddress.getText().toString());
+        cv.put(Students.STUDENT_PHONE, etStudPhone.getText().toString());
+        cv.put(Students.HOME_PHONE, etHomePhone.getText().toString());
+        cv.put(Students.MOTHER_NAME, etMomName.getText().toString());
+        cv.put(Students.MOTHER_PHONE, etMomPhone.getText().toString());
+        cv.put(Students.FATHER_NAME, etDadName.getText().toString());
+        cv.put(Students.FATHER_PHONE, etDadPhone.getText().toString());
+
+        // Inserts the data to the table
+        db = hlp.getWritableDatabase();
+        db.insert(Students.TABLE_STUDENTS, null, cv);
+        db.close();
+
+        Toast.makeText(this, "Added new student!", Toast.LENGTH_SHORT).show();
+    }
+
+    public boolean areEmptyFields() {
+        return (etStudName.getText().toString().equals("")) ||
+                (etAddress.getText().toString().equals("")) ||
+                (etStudPhone.getText().toString().equals("")) ||
+                (etHomePhone.getText().toString().equals("")) ||
+                (etMomName.getText().toString().equals("")) ||
+                (etMomPhone.getText().toString().equals("")) ||
+                (etDadName.getText().toString().equals("")) ||
+                (etDadPhone.getText().toString().equals(""));
     }
 }
