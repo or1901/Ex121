41a622ce8793f28f30f3ddaa7165f4df1b488219
Ori Roitzaid <oriroitzaid@gmail.com>
2024-01-04 22:30:31 +0200

    Added activity of showing grades, and made its initial design

---
+++ app/src/main/java/com/example/ex121/ShowGradesActivity.java
@@ -0,0 +1,14 @@
+package com.example.ex121;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowGradesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_grades);
+    }
+}
