2883c50871fc8aa7d84d7db0d1b15083350fef92
Ori Roitzaid <oriroitzaid@gmail.com>
2024-01-05 18:16:48 +0200

    Finished the design of the filtering data activity, and coded the spinner of filtering options

--- app/src/main/java/com/example/ex121/SortAndFilterActivity.java
+++ app/src/main/java/com/example/ex121/SortAndFilterActivity.java
@@ -3,13 +3,32 @@ package com.example.ex121;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
-public class SortAndFilterActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class SortAndFilterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     Intent gi;
+    SQLiteDatabase db;
+    HelperDB hlp;
+    Cursor crsr;
+    ContentValues cv;
+    ArrayAdapter<String> adpFilterOptions;
+    Spinner spinFilterOptions;
+    String[] filterOptions = {"Grades of all students in a given quarter",
+            "Grades of all students in a given subject",
+            "All grades of a given student in a decreasing order"};
+    int selectedFilterOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +36,37 @@ public class SortAndFilterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_sort_and_filter);
 
         gi = getIntent();
+        initAll();
+    }
+
+    /**
+     * This function initializes the views and the database.
+     */
+    private void initAll() {
+        // Inits the views
+        spinFilterOptions = findViewById(R.id.spinFilterOptions);
+        spinFilterOptions.setOnItemSelectedListener(this);
+
+        // Inits the database
+        hlp = new HelperDB(this);
+        db = hlp.getWritableDatabase();
+        db.close();
+
+        cv = new ContentValues();
+
+        adpFilterOptions = new ArrayAdapter<String>(this,
+                androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, filterOptions);
+        spinFilterOptions.setAdapter(adpFilterOptions);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 
     /**
@@ -37,25 +87,19 @@ public class SortAndFilterActivity extends AppCompatActivity {
      * @param item the menu item that was selected.
      * @return must return true for the menu to react.
      */
-    public boolean onOptionsItemSelected(@NonNull MenuItem item){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
 
-        if(id == R.id.menuAddStudent){
+        if (id == R.id.menuAddStudent) {
             gi.setClass(this, InputStudentActivity.class);
             startActivity(gi);
-        }
-        else if(id == R.id.menuAddGrade)
-        {
+        } else if (id == R.id.menuAddGrade) {
             gi.setClass(this, InputGradeActivity.class);
             startActivity(gi);
-        }
-        else if(id == R.id.menuShowStudents)
-        {
+        } else if (id == R.id.menuShowStudents) {
             gi.setClass(this, ShowStudentsActivity.class);
             startActivity(gi);
-        }
-        else if(id == R.id.menuShowGrades)
-        {
+        } else if (id == R.id.menuShowGrades) {
             gi.setClass(this, ShowGradesActivity.class);
             startActivity(gi);
         }
